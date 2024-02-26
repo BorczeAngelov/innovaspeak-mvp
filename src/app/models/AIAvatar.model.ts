b31267373ee9818e86dfdb3808f121ae068207a9
Borcze Angelov <Borcze.Angelov@yahoo.com>
2024-02-26 08:23:06 +0100

    UX improvements

--- src/app/models/AIAvatar.model.ts
+++ src/app/models/AIAvatar.model.ts
@@ -23,9 +23,14 @@ export class GraceModel extends AIAvatarModel {
   
     constructor() {
         const name = 'Grace';
-        const subtitle = 'Your Daily Reflection Companion';
+        const subtitle = 'Daily Reflection AI Companion';
         const imageUri = 'assets/images/avatar-grace.jpg';
-        const longText = "As your Daily Reflection Companion, I am dedicated to offering you an experience that encompasses deep and meaningful conversations. Through attentive listening and thoughtful questioning, I provide a safe space for you to share your thoughts and feelings. My approach includes guiding you towards understanding and growth, ensuring that our interactions are filled with positive reflections. Each conversation is personalized to meet your unique needs, all within a positive and supportive environment. My ultimate goal is to make our conversations a rewarding experience for you, where you feel supported as we explore and understand the day's events and emotions together. This journey will lead to insightful reflections and a deeper understanding of yourself.";
+        const longText = 
+        `Hi, I'm Grace, your Virtual AI Companion.
+        Think of me as a friend who's always there to listen, understand, and guide you through a journey of self-discovery.
+        With just a question, we'll dive deep into your day, exploring moments that made you smile or pause.
+        Ready for a heart-to-heart? Let's talk about your day.`;
+
         // prompt for image
         // const longText = "As your Daily Reflection Companion, here's what I offer to you: Deep, Meaningful Conversations; Listening and Questioning; A Safe Space for Sharing; Guidance for Understanding and Growth; Positive Reflections; Personalized Conversations; A Positive and Supportive Environment. My goal is to make our conversations a rewarding experience for you, where you feel supported in exploring and understanding your day's events and emotions, leading to insightful reflections and a deeper understanding of yourself.";
         const personalityTraits = [
